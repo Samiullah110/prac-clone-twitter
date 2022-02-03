39dc372af0338bcf629030150b39744c97592ae4
samiullah <sami.ullah@fff>
2022-02-03 11:18:31 +0500

    9 commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Modal from '../components/Modal'
 import { useRecoilState } from 'recoil'
 import { modalState } from '../atoms/modalAtom'
 import Widget from '../components/Widget'
-import SignIn from './api/signin'
+import SignIn from './auth/signin'
 
 export default function Home({ trendingResults, followResults, providers }) {
   const { data: session } = useSession();
